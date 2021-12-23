b6e61c52341f202c521232f3ef240e53b3ddb197
android <csi2@grupogema.com.co>
2021-12-23 16:32:15 -0500

    ajustes Dashboard

--- app/src/main/java/com/allservicerhyno/aplicacion/dashboard/Main.kt
+++ app/src/main/java/com/allservicerhyno/aplicacion/dashboard/Main.kt
@@ -1,139 +1,33 @@
-package com.allservicerhyno.aplicacion.dashboard;
+package com.allservicerhyno.aplicacion.dashboard
 
-import androidx.annotation.ColorInt;
-import androidx.annotation.ColorRes;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.allservicerhyno.aplicacion.R
+import com.allservicerhyno.aplicacion.dashboard.Home
 
-import android.app.Fragment;
-import android.content.res.TypedArray;
-import android.graphics.drawable.Drawable;
-import android.os.Bundle;
-
-import com.allservicerhyno.aplicacion.R;
-import com.yarolegovich.slidingrootnav.SlidingRootNav;
-import com.yarolegovich.slidingrootnav.SlidingRootNavBuilder;
-
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
-public class Main extends AppCompatActivity implements DrawerAdapter.OnItemSelectedListener {
-
-    private static final int POS_CLOSE = 0;
-    private static final int POS_DASHBOARD = 1;
-    private static final int POS_MY_PROFILE = 2;
-    private static final int POS_SETTINGS = 3;
-    private static final int POS_LOGOUT = 5;
-
-    private String[] screenTittles;
-    private Drawable[] screenIcons;
-
-
-    private SlidingRootNav slidingRootnav;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        slidingRootnav = new SlidingRootNavBuilder(this)
-                .withDragDistance(180)
-                .withRootViewScale(0.75f)
-                .withRootViewElevation(25)
-                .withToolbarMenuToggle(toolbar)
-                .withMenuOpened(false)
-                .withContentClickableWhenMenuOpened(false)
-                .withSavedState(savedInstanceState)
-                .withMenuLayout(R.layout.drawer_menu)
-                .inject();
-
-        screenIcons = loadScreenIcons();
-        screenTittles = loadScreenTittles();
-
-        DrawerAdapter adapter = new DrawerAdapter(Arrays.asList(
-                createItemFor(POS_CLOSE),
-                createItemFor(POS_DASHBOARD).setChecked(true),
-                createItemFor(POS_MY_PROFILE),
-                createItemFor(POS_SETTINGS),
-                new SpaceItem(260),
-                createItemFor(POS_LOGOUT)
-        ));
-        adapter.setListener(this);
-
-        RecyclerView list = findViewById(R.id.drawer_list);
-        list.setNestedScrollingEnabled(false);
-        list.setLayoutManager(new LinearLayoutManager(this));
-        list.setAdapter(adapter);
-
-        adapter.setSelected(POS_DASHBOARD);
-
-    }
-
-    private DrawerItem createItemFor(int position) {
-        return new SimpleItem(screenIcons[position], screenTittles[position])
-                .withIconTint(color(R.color.light_gray))
-                .withTextTint(color(R.color.black))
-                .withSelectedIconTint(color(R.color.light_gray))
-                .withSelectedTextTint(color(R.color.light_gray));
-    }
-
-    @ColorInt
-    private int color(@ColorRes int res) {
-        return ContextCompat.getColor(this, res);
-    }
-
-
-    private String[] loadScreenTittles() {
-        return getResources().getStringArray(R.array.id_activityScreenTittles);
-    }
-
-    private Drawable[] loadScreenIcons() {
-        TypedArray ta = getResources().obtainTypedArray(R.array.id_activityScreenIcon);
-        Drawable[] icons = new Drawable[ta.length()];
-        for (int i = 0; i < ta.length(); i++) {
-            int id = ta.getResourceId(i, 0);
-            if (id != 0) {
-                icons[i] = ContextCompat.getDrawable(this, id);
+class Main : AppCompatActivity() {
+    private var bottomBar: BottomNavigationView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+        replace(Home())
+        val bottomBar = findViewById<BottomNavigationView>(R.id.bottomBar)
+        bottomBar.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> replace(Home())
+                R.id.account -> replace(Account())
+                R.id.settings -> replace(Settings())
+                R.id.notifitication -> replace(Notification())
             }
-        }
-        ta.recycle();
-        return icons;
+            true
+        })
     }
 
-    @Override
-    public void onBackPressed() {
-        finish();
-    }
-
-    @Override
-    public void onItemSelected(int position) {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-
-        if (position == POS_DASHBOARD) {
-            DashBoardFragment dashBoardFragment = new DashBoardFragment();
-            transaction.replace(R.id.container, dashBoardFragment);
-        } else if (position == POS_MY_PROFILE) {
-            MyProfileFragment myProfileFragment = new MyProfileFragment();
-            transaction.replace(R.id.container, myProfileFragment);
-        } else if (position == POS_SETTINGS) {
-            SettingsFragment settingsFragment = new SettingsFragment();
-            transaction.replace(R.id.container, settingsFragment);
-        } else if (position == POS_LOGOUT) {
-            finish();
-
-        }
-
-
-        slidingRootnav.closeMenu();
-        transaction.addToBackStack(null);
-        transaction.commit();
+    private fun replace(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frame, fragment)
+        transaction.commit()
     }
 }
